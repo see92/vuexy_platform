19c012cae48f95b33cf62f17703fab8fec232e64
unknown <906437853@qq.com>
2022-12-27 00:32:12 +0800

    文件外发审计

--- src/views/document/pages/doc_outgo.vue
+++ src/views/document/pages/doc_outgo.vue
@@ -49,15 +49,48 @@
             </div>
           </b-form-group>
           <div v-show="this.control.rules == 1">
+            <!-- 文件类型 -->
             <b-form-group label="" label-for="" label-cols-md="auto">
               <b-form-checkbox v-model="control.fileType">
                 文件类型
               </b-form-checkbox>
             </b-form-group>
-            {{ control.fileType }}
             <b-form-group label="" label-for="" label-cols-md="auto">
               <div style="width: 550px">
-                <v-select :reduce="(title) => title.key" label="title" />
+                <v-select
+                  :disabled="!control.fileType"
+                  :reduce="(title) => title.key"
+                  label="title"
+                />
+              </div>
+            </b-form-group>
+            <!-- 文件大小 -->
+            <b-form-group label="" label-for="" label-cols-md="auto">
+              <b-form-checkbox v-model="control.fileSize">
+                文件大小
+              </b-form-checkbox>
+            </b-form-group>
+            <b-form-group label="" label-for="" label-cols-md="auto">
+              <div style="width: 550px" class="d-flex">
+                <div class="mr-1">
+                  <el-input-number
+                    style="width: 150px"
+                    controls-position="right"
+                    v-model="control.minNum"
+                    :min="0"
+                    :max="16777216"
+                  />
+                </div>
+                <div class="mr-1 vSelect">
+                  <v-select
+                    :options="unitOptions"
+                    :searchable="false"
+                    :clearable="false"
+                    :reduce="(title) => title.key"
+                    label="title"
+                    v-model="control.minUnit"
+                  />
+                </div>
               </div>
             </b-form-group>
           </div>
@@ -87,7 +120,7 @@ import {
 } from "bootstrap-vue";
 import Input from "./components/b-input.vue";
 import vSelect from "vue-select";
-import { specifyRouteOptions, rulesOptions } from "./js/options";
+import { specifyRouteOptions, rulesOptions, unitOptions } from "./js/options";
 export default {
   name: "doc_outgo",
   components: {
@@ -104,11 +137,14 @@ export default {
       showModal: false, //弹窗
       specifyRouteOptions, //应用选项
       rulesOptions, //规则选项
+      unitOptions, //文件大小选项
       control: {
         app: [],
         custom: "",
         rules: 0,
-        fileType: 0,
+        fileType: false,
+        minNum: 0, //最小值
+        minUnit: 1, //最小单位
       },
     };
   },
