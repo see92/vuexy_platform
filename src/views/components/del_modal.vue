771d63a40bc47ec861a7efb61f82f4b62b5ad07f
see92 <dsq_1105@outlook.com>
2022-12-26 18:01:13 +0800

    文件外发管控

---
+++ src/views/components/del_modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-modal
+    size="sm"
+    title="提示："
+    centered
+    hide-footer
+    no-close-on-backdrop
+    no-close-on-esc
+    :visible.sync="visible"
+    @close="ModalReset"
+  >
+    <div>此操作将会删除数据，是否继续？</div>
+    <div class="d-flex justify-content-end">
+      <slot name="ok" />
+      <slot name="cancel" />
+    </div>
+  </b-modal>
+</template>
+
+<script>
+import { BModal } from "bootstrap-vue";
+export default {
+  name: "Modal",
+  components: {
+    BModal,
+  },
+  props: {
+    ModalFlag: {
+      default: false,
+    },
+  },
+  data: () => ({
+    visible: false,
+  }),
+  methods: {
+    ModalReset() {
+      this.$emit("update:ModalFlag", false);
+    },
+  },
+  watch: {
+    ModalFlag() {
+      this.visible = this.ModalFlag;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/document/pages/doc_outgo.vue
+++ src/views/document/pages/doc_outgo.vue
@@ -36,12 +36,38 @@
               v-model="control.custom"
             />
           </b-form-group>
+          <b-form-group label="规则：" label-for="rules" label-cols-md="auto">
+            <div style="width: 550px">
+              <v-select
+                :options="rulesOptions"
+                :searchable="false"
+                :clearable="false"
+                :reduce="(title) => title.key"
+                label="title"
+                v-model="control.rules"
+              />
+            </div>
+          </b-form-group>
+          <div v-show="this.control.rules == 1">
+            <b-form-group label="" label-for="" label-cols-md="auto">
+              <b-form-checkbox v-model="control.fileType">
+                文件类型
+              </b-form-checkbox>
+            </b-form-group>
+            {{ control.fileType }}
+            <b-form-group label="" label-for="" label-cols-md="auto">
+              <div style="width: 550px">
+                <v-select :reduce="(title) => title.key" label="title" />
+              </div>
+            </b-form-group>
+          </div>
         </div>
-        <b-form-group label="规则：" label-for="rules" label-cols-md="auto">
+
+        <!-- <b-form-group label="规则：" label-for="rules" label-cols-md="auto">
           <div style="width: 550px">
             <v-select :options="rulesOptions" />
           </div>
-        </b-form-group>
+        </b-form-group> -->
         <div class="d-flex justify-content-end">
           <b-button variant="primary" class="mr-1">保存</b-button>
           <b-button variant="outline-primary">取消</b-button>
@@ -52,13 +78,27 @@
 </template>
 
 <script>
-import { BButton, BModal, BFormGroup, BFormTextarea } from "bootstrap-vue";
+import {
+  BButton,
+  BModal,
+  BFormGroup,
+  BFormTextarea,
+  BFormCheckbox,
+} from "bootstrap-vue";
 import Input from "./components/b-input.vue";
 import vSelect from "vue-select";
 import { specifyRouteOptions, rulesOptions } from "./js/options";
 export default {
   name: "doc_outgo",
-  components: { BButton, BModal, Input, vSelect, BFormGroup, BFormTextarea },
+  components: {
+    BButton,
+    BModal,
+    Input,
+    vSelect,
+    BFormGroup,
+    BFormTextarea,
+    BFormCheckbox,
+  },
   data() {
     return {
       showModal: false, //弹窗
@@ -67,6 +107,8 @@ export default {
       control: {
         app: [],
         custom: "",
+        rules: 0,
+        fileType: 0,
       },
     };
   },
